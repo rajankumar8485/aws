5eb9363e53257fc8ee2b83f9c32faeec4a8cea92
Rajan Kumar <m1082141@mindtree.com>
2022-10-21 22:38:41 +0530

    push

--- cronjob_lambda/variables.tf
+++ cronjob_lambda/variables.tf
@@ -1,10 +1,12 @@
 variable "stack_name" {
   description = "A name generated for the stack."
   type        = string
+  default = "test"
 }
 
 variable "lambda_handler" {
   type    = string
+  default = "lambda_handler"
 }
 
 variable "lambda_runtime" {
@@ -15,6 +17,7 @@ variable "lambda_runtime" {
 variable "event_rule_description" {
   description = "Description for event rule"
   type        = string
+  default = "test"
 }
 
 variable "tags" {
@@ -27,4 +30,5 @@ variable "tags" {
 variable "aws_region" {
   description = "Working region"
   type        = string
+  default = "us-east-1"
 }
---
+++ mongo.tf
@@ -0,0 +1,10 @@
+peering_map = [
+    {
+        cidr_block = ""
+        cidr_name  = "atlas-cidr1"
+    },
+    {
+        cidr_block = ""
+        cidr_name  = "atlas-cidr1"
+    }
+]
